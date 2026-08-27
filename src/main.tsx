b16d9a8dada6bb7a6a716ac8defdf3882a01a74b
dennismwangi1 <denniskamau1803@gmail.com>
2026-08-27 15:31:36 +0300

    chore: configure andishi

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,22 @@
-
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App.tsx";
 import "./index.css";
-import { FormspreeProvider } from '@formspree/react';
-
-
+import { FormspreeProvider } from "@formspree/react";
+import { AndishiProvider } from "@andishi/react";
 createRoot(document.getElementById("root")!).render(
-    <BrowserRouter>
-        <FormspreeProvider project="2860308000625655395">
-            <App />
-        </FormspreeProvider>
-    </BrowserRouter>
+  <BrowserRouter>
+    <FormspreeProvider project="2860308000625655395">
+      <AndishiProvider
+        siteId={
+          import.meta.env.ANDISHI_SITE_ID
+        }
+        publishableKey={
+          import.meta.env.ANDISHI_PUBLISHABLE_KEY
+        }
+      >
+        <App />
+      </AndishiProvider>
+    </FormspreeProvider>
+  </BrowserRouter>,
 );
